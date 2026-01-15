f81e5d948645b14bf680077ed78a4b9df43d511c
GrzegorzMystek <krulbaton120@gmail.com>
2026-01-15 22:52:01 +0100

    Add files via upload

---
+++ funkcje-ulepszenia.js
@@ -0,0 +1,623 @@
+function u1(){
+    if(liczbaklikniec >= 50){
+        liczbaklikniec -= 50;
+        boost = 2;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p1").classList.replace("block","none");
+        kupioneK.p1 = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}   
+
+function u2(){
+    if(liczbaklikniec >= 566){
+        liczbaklikniec -= 566;
+        boost = 4;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p2").classList.replace("block","none");
+        kupioneK.p2 = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function u3(){
+    if(liczbaklikniec >= 2338){
+        liczbaklikniec -= 2338;
+        boost = 7;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p3").classList.replace("block","none");
+        kupioneK.p3 = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+function u4(){
+    if(liczbaklikniec >= 6400){
+        liczbaklikniec -= 6400;
+        boost = 12;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p4").classList.replace("block","none");
+        kupioneK.p4 = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function u5(){
+    if(liczbaklikniec >= 13975){
+        liczbaklikniec -= 13975;
+        boost = 18;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p5").classList.replace("block","none");
+        kupioneK.p5 = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function u6(){
+    if(liczbaklikniec >= 26454){
+        liczbaklikniec -= 26454;
+        boost = 30;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p6").classList.replace("block","none");
+        kupioneK.p6 = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function u7(){
+    if(liczbaklikniec >= 45375){
+        liczbaklikniec -= 45375;
+        boost = 45;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p7").classList.replace("block","none");
+        kupioneK.p7 = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function u8(){
+    if(liczbaklikniec >= 72408){
+        liczbaklikniec -= 72408;
+        boost = 80;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p8").classList.replace("block","none");
+        kupioneK.p8 = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function u9(){
+    if(liczbaklikniec >= 109350){
+        liczbaklikniec -= 109350;
+        boost = 122;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p9").classList.replace("block","none");
+        kupioneK.p9 = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function u10(){
+    if(liczbaklikniec >= 158114){
+        liczbaklikniec -= 158114;
+        boost = 160;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p10").classList.replace("block","none");
+        kupioneK.p10 = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function u11(){
+    if(liczbaklikniec >= 220721){
+        liczbaklikniec -= 220721;
+        boost = 200;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p11").classList.replace("block","none");
+        kupioneK.p11 = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function u12(){
+    if(liczbaklikniec >= 299298){
+        liczbaklikniec -= 299298;
+        boost = 300;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p12").classList.replace("block","none");
+        kupioneK.p12 = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function u13(){
+    if(liczbaklikniec >= 396070){
+        liczbaklikniec -= 396070;
+        boost = 420;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p13").classList.replace("block","none");
+        kupioneK.p13 = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function u14(){
+    if(liczbaklikniec >= 513355){
+        liczbaklikniec -= 513355;
+        boost = 550;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p14").classList.replace("block","none");
+        kupioneK.p14 = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function u15(){
+    if(liczbaklikniec >= 653566){
+        liczbaklikniec -= 653566;
+        boost = 777;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p15").classList.replace("block","none");
+        kupioneK.p15 = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function u16(){
+    if(liczbaklikniec >= 819200){
+        liczbaklikniec -= 819200;
+        boost = 900;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p16").classList.replace("block","none");
+        kupioneK.p16 = true;
+        saveGame();
+    } else alert("Nie masz wystarczająco dużo 🐱");
+}
+
+function u17(){
+    if(liczbaklikniec >= 1012841){
+        liczbaklikniec -= 1012841;
+        boost = 1111;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p17").classList.replace("block","none");
+        kupioneK.p17 = true;
+        saveGame();
+    } else alert("Nie masz wystarczająco dużo 🐱");
+}
+
+function u18(){
+    if(liczbaklikniec >= 1237154){
+        liczbaklikniec -= 1237154;
+        boost = 1350;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p18").classList.replace("block","none");
+        kupioneK.p18 = true;
+        saveGame();
+    } else alert("Nie masz wystarczająco dużo 🐱");
+}
+
+function u19(){
+    if(liczbaklikniec >= 1494884){
+        liczbaklikniec -= 1494884;
+        boost = 1550;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p19").classList.replace("block","none");
+        kupioneK.p19 = true;
+        saveGame();
+    } else alert("Nie masz wystarczająco dużo 🐱");
+}
+
+function u20(){
+    if(liczbaklikniec >= 1788854){
+        liczbaklikniec -= 1788854;
+        boost = 1800;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p20").classList.replace("block","none");
+        kupioneK.p20 = true;
+        saveGame();
+    } else alert("Nie masz wystarczająco dużo 🐱");
+}
+
+function u21(){
+    if(liczbaklikniec >= 2121962){
+        liczbaklikniec -= 2121962;
+        boost = 2000;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p21").classList.replace("block","none");
+        kupioneK.p21 = true;
+        saveGame();
+    } else alert("Nie masz wystarczająco dużo 🐱");
+}
+
+function u22(){
+    if(liczbaklikniec >= 2497177){
+        liczbaklikniec -= 2497177;
+        boost = 2222;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p22").classList.replace("block","none");
+        kupioneK.p22 = true;
+        saveGame();
+    } else alert("Nie masz wystarczająco dużo 🐱");
+}
+
+function u23(){
+    if(liczbaklikniec >= 2917544){
+        liczbaklikniec -= 2917544;
+        boost = 2500;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p23").classList.replace("block","none");
+        kupioneK.p23 = true;
+        saveGame();
+    } else alert("Nie masz wystarczająco dużo 🐱");
+}
+
+function u24(){
+    if(liczbaklikniec >= 3386175){
+        liczbaklikniec -= 3386175;
+        boost = 2900;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p24").classList.replace("block","none");
+        kupioneK.p24 = true;
+        saveGame();
+    } else alert("Nie masz wystarczająco dużo 🐱");
+}
+
+function u25(){
+    if(liczbaklikniec >= 3906250){
+        liczbaklikniec -= 3906250;
+        boost = 3333;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p25").classList.replace("block","none");
+        kupioneK.p25 = true;
+        saveGame();
+    } else alert("Nie masz wystarczająco dużo 🐱");
+}
+
+function u26(){
+    if(liczbaklikniec >= 4481018){
+        liczbaklikniec -= 4481018;
+        boost = 3700;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p26").classList.replace("block","none");
+        kupioneK.p26 = true;
+        saveGame();
+    } else alert("Nie masz wystarczająco dużo 🐱");
+}
+
+function u27(){
+    if(liczbaklikniec >= 5113793){
+        liczbaklikniec -= 5113793;
+        boost = 4200;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p27").classList.replace("block","none");
+        kupioneK.p27 = true;
+        saveGame();
+    } else alert("Nie masz wystarczająco dużo 🐱");
+}
+
+function u28(){
+    if(liczbaklikniec >= 5807953){
+        liczbaklikniec -= 5807953;
+        boost = 4560;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p28").classList.replace("block","none");
+        kupioneK.p28 = true;
+        saveGame();
+    } else alert("Nie masz wystarczająco dużo 🐱");
+}
+
+function u29(){
+    if(liczbaklikniec >= 6566939){
+        liczbaklikniec -= 6566939;
+        boost = 5000;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p29").classList.replace("block","none");
+        kupioneK.p29 = true;
+        saveGame();
+    } else alert("Nie masz wystarczająco dużo 🐱");
+}
+
+function u30(){
+    if(liczbaklikniec >= 7394255){
+        liczbaklikniec -= 7394255;
+        boost = 5555;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#p30").classList.replace("block","none");
+        kupioneK.p30 = true;
+        saveGame();
+    } else alert("Nie masz wystarczająco dużo 🐱");
+}
+// Ulepszenia szansy na krytyka
+
+function uc1(){
+    if(liczbaklikniec >= 1000){
+        liczbaklikniec -= 1000;
+        critChance = 0.01;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cc-1").classList.replace("block","none");
+        kupioneCc['cc-1'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function uc2(){
+    if(liczbaklikniec >= 11314){
+        liczbaklikniec -= 11314;
+        critChance = 0.02;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cc-2").classList.replace("block","none");
+        kupioneCc['cc-2'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function uc3(){
+    if(liczbaklikniec >= 46765){
+        liczbaklikniec -= 46765;
+        critChance = 0.03;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cc-3").classList.replace("block","none");
+        kupioneCc['cc-3'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function uc4(){
+    if(liczbaklikniec >= 128000){
+        liczbaklikniec -= 128000;
+        critChance = 0.04;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cc-4").classList.replace("block","none");
+        kupioneCc['cc-4'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function uc5(){
+    if(liczbaklikniec >= 279508){
+        liczbaklikniec -= 279508;
+        critChance = 0.05 ;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cc-5").classList.replace("block","none");
+        kupioneCc['cc-5'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function uc6(){
+    if(liczbaklikniec >= 529090){
+        liczbaklikniec -= 529090;
+        critChance = 0.06 ;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cc-6").classList.replace("block","none");
+        kupioneCc['cc-6'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function uc7(){
+    if(liczbaklikniec >= 907493){
+        liczbaklikniec -= 907493;
+        critChance = 0.07 ;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cc-7").classList.replace("block","none");
+        kupioneCc['cc-7'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function uc8(){
+    if(liczbaklikniec >= 1448155){
+        liczbaklikniec -= 1448155;
+        critChance = 0.08 ;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cc-8").classList.replace("block","none");
+        kupioneCc['cc-8'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function uc9(){
+    if(liczbaklikniec >= 2187000){
+        liczbaklikniec -= 2187000;
+        critChance = 0.09 ;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cc-9").classList.replace("block","none");
+        kupioneCc['cc-9'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function uc10(){
+    if(liczbaklikniec >= 3162278){
+        liczbaklikniec -= 3122278;
+        critChance = 0.1 ;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cc-10").classList.replace("block","none");
+        kupioneCc['cc-10'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+//funkcje dla mnożników
+
+function um1(){
+    if(liczbaklikniec >= 10000){
+        liczbaklikniec -= 10000;
+        critMultiplier += 2;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cm-1").classList.replace("block","none");
+        kupioneCm['cm-1'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function um2(){
+    if(liczbaklikniec >= 56569){
+        liczbaklikniec -= 56569;
+        critMultiplier += 2;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cm-2").classList.replace("block","none");
+        kupioneCm['cm-2'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function um3(){
+    if(liczbaklikniec >= 155885){
+        liczbaklikniec -= 155885;
+        critMultiplier += 2;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cm-3").classList.replace("block","none");
+        kupioneCm['cm-3'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function um4(){
+    if(liczbaklikniec >= 320000){
+        liczbaklikniec -= 320000;
+        critMultiplier += 2;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cm-4").classList.replace("block","none");
+        kupioneCm['cm-4'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function um5(){
+    if(liczbaklikniec >= 559017){
+        liczbaklikniec -= 559017;
+        critMultiplier += 2;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cm-5").classList.replace("block","none");
+        kupioneCm['cm-5'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function um6(){
+    if(liczbaklikniec >= 881816){
+        liczbaklikniec -= 881816;
+        critMultiplier += 2;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cm-6").classList.replace("block","none");
+        kupioneCm['cm-6'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function um7(){
+    if(liczbaklikniec >= 1296418){
+        liczbaklikniec -= 1296418;
+        critMultiplier += 2;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cm-7").classList.replace("block","none");
+        kupioneCm['cm-7'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function um8(){
+    if(liczbaklikniec >= 1810193){
+        liczbaklikniec -= 1810193;
+        critMultiplier += 2;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cm-8").classList.replace("block","none");
+        kupioneCm['cm-8'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function um9(){
+    if(liczbaklikniec >= 2430000){
+        liczbaklikniec -= 2430000;
+        critMultiplier += 2;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cm-9").classList.replace("block","none");
+        kupioneCm['cm-9'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+}
+
+function um10(){
+    if(liczbaklikniec >= 3162278){
+        liczbaklikniec -= 3162278;
+        critMultiplier += 2;
+        document.getElementById("liczba").textContent = liczbaklikniec + "🐱";
+        document.querySelector("#cm-10").classList.replace("block","none");
+        kupioneCm['cm-10'] = true;
+        saveGame();
+    } else {
+        alert("Nie masz wystarczająco dużo 🐱");
+    }
+
+}
+
